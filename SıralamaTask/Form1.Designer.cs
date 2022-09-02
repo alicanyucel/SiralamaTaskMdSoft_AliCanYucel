e38ef51d5763b48ab11a54f438058ecec8211cde
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2022-09-02 21:23:34 +0300

    Add project files.

---
+++ SıralamaTask/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SıralamaTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchtxt1 = new System.Windows.Forms.RichTextBox();
+            this.btnSayiUret = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rchtxt1
+            // 
+            this.rchtxt1.Location = new System.Drawing.Point(12, 103);
+            this.rchtxt1.Name = "rchtxt1";
+            this.rchtxt1.Size = new System.Drawing.Size(250, 272);
+            this.rchtxt1.TabIndex = 0;
+            this.rchtxt1.Text = "";
+            this.rchtxt1.TextChanged += new System.EventHandler(this.rchtxt1_TextChanged);
+            // 
+            // btnSayiUret
+            // 
+            this.btnSayiUret.Location = new System.Drawing.Point(26, 48);
+            this.btnSayiUret.Name = "btnSayiUret";
+            this.btnSayiUret.Size = new System.Drawing.Size(167, 23);
+            this.btnSayiUret.TabIndex = 1;
+            this.btnSayiUret.Text = "Sayıları Üret";
+            this.btnSayiUret.UseVisualStyleBackColor = true;
+            this.btnSayiUret.Click += new System.EventHandler(this.btnSayiUret_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 450);
+            this.Controls.Add(this.btnSayiUret);
+            this.Controls.Add(this.rchtxt1);
+            this.Name = "Form1";
+            this.Text = "Sıralama Algoritması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox rchtxt1;
+        private Button btnSayiUret;
+    }
+}
---
+++ SıralamaTask/Form1.cs
@@ -0,0 +1,81 @@
+namespace SıralamaTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rchtxt1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSayiUret_Click(object sender, EventArgs e)
+        {
+
+
+            MessageBox.Show("Sıralama algoritması için selection sort ve quic sort kullanılacak ");
+            Random random = new Random(); // 1 mil yon sayıyı rastgele üretecek.
+            int[] sayilar = new int[1000];
+            rchtxt1.Text += "uretilen sayilar" + "\n";
+            for (int i = 0; i < 1000; i++) // pozitif sayı sıralasın 
+            {
+                int number = random.Next(1, 2147483647); // intin maksimum alacağı değer girildi üretsin bakalım :)
+                sayilar[i] = number;
+            }
+            foreach (int item in sayilar)
+            {  // üretilen sayıları bir richtexboxa attım
+                rchtxt1.Text += item.ToString() + "\n";
+            }
+            rchtxt1.Text += "Selection Sort ile Sıralı Liste" + "\n";
+            // selection sort ile sıralasın
+            int gecici, ek;
+            for (int i = 0; i < 1000; i++)
+            {
+                ek = i; // dizinin ilk elemanı en kucuk eleman olarak kabul et ve karşılaştır
+                for (int j = i + 1; j < 1000; j++)
+                {
+                    if (sayilar[j] < sayilar[ek])
+                    {
+                        ek = j;
+                    }
+                }
+                gecici = sayilar[ek];
+                sayilar[ek] = sayilar[i];
+                sayilar[i] = gecici;
+            }
+            // sıralı listeyi görelim
+            foreach (int item in sayilar)
+            {
+                rchtxt1.Text += item.ToString() + "\n";
+            }
+            rchtxt1.Text += "buble sort algoritmasi ile siralama" + "\n";
+            // buble sortla da sıralayalım
+
+            for (int j = 0; j <= 998; j++)
+            {
+                for (int i = 0; i <= 998; i++)
+                {
+                    if (sayilar[i] > sayilar[i + 1])
+                    {
+                        gecici =sayilar[i + 1];
+                        sayilar[i + 1] = sayilar[i];
+                        sayilar[i] = gecici;
+                    }
+                }
+            }
+            foreach (var item in sayilar)
+            {
+                rchtxt1.Text += item.ToString() + "\n";
+            }
+            // sıralanan diziyi richtextboxa yazdırıyoruz....
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ali Can Yücel- Yazılım Yüksek Mühendisi - Ahmet Yesevi Üniversitesi");
+        }
+    }
+}
